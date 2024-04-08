fad472221f6346b9b6a4b587d2c7216493d0421d
Kevin <kevinlai29@outlook.com>
2024-04-08 14:50:03 +0800

    ruins2.0

--- ruins-next/pages/_app.js
+++ ruins-next/pages/_app.js
@@ -1,18 +1,17 @@
-import BoardsContextProvider from "@/contexts/use-boards";
-import TogglesContextProvider from "@/contexts/use-toggles";
-import "@/styles/globals.css";
-import { CartProvider } from "@/hooks/use-cart";
+import BoardsContextProvider from '@/contexts/use-boards'
+import TogglesContextProvider from '@/contexts/use-toggles'
+import '@/styles/globals.css'
+import { CartProvider } from '@/hooks/use-cart'
 
 export default function App({ Component, pageProps }) {
-   const getLayout = Component.getLayout ?? ((page) => page);
+  const getLayout = Component.getLayout ?? ((page) => page)
   return (
     <CartProvider>
-    <BoardsContextProvider>
-      <TogglesContextProvider>
-        {getLayout(<Component {...pageProps} />)}/
-      </TogglesContextProvider>
-    </BoardsContextProvider>
+      <BoardsContextProvider>
+        <TogglesContextProvider>
+          {getLayout(<Component {...pageProps} />)}/
+        </TogglesContextProvider>
+      </BoardsContextProvider>
     </CartProvider>
-  );
- 
+  )
 }
