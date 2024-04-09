9e4bd1f3aca367b6c29c106c52cff9b6f608cdbe
Kevin <kevinlai29@outlook.com>
2024-04-09 16:04:59 +0800

    add sample dev

--- ruins-next/components/config/api-path.js
+++ ruins-next/components/config/api-path.js
@@ -4,3 +4,9 @@ export const API_SERVER = "http://localhost:3001"
 export const MB_SIGNUP = `${API_SERVER}/member/signup`
 // login
 export const MB_LOGIN = `${API_SERVER}/member/login`
+
+//商品列表
+export const PRODUCT_LIST = `${API_SERVER}/product/api`
+
+//單一商品
+export const PRODUCT_ONE = `${API_SERVER}/product/api/getProduct`
