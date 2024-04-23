426836f07bb066ce29d061c1c96aaa21d6cd1927
tomatoyota <firemouse2020@gmail.com>
2024-04-23 10:23:22 +0800

    game KeyboardControls

--- ruins-next/components/ellie/three/Experience.js
+++ ruins-next/components/ellie/three/Experience.js
@@ -1,20 +1,40 @@
 import React,{ useState, useRef, useEffect } from 'react'
-import { Box, OrbitControls } from '@react-three/drei'
+import { Box, OrbitControls, useKeyboardControls } from '@react-three/drei'
 import { CylinderCollider, RigidBody } from '@react-three/rapier'
 import { Cylinder } from '@react-three/drei';
 import GirlController  from '../character/GirlController';
+import { useFrame } from '@react-three/fiber';
+import Ball from './ball';
 // import Box1 from './box';
 
 export default function Experience() {
   const [hover, setHover ] = useState(false);
+  // const [state, setState] = useState({
+  //   controls: {
+  //     left: false,
+  //     right: false,
+  //   },
+  // });
   const cube = useRef();
   const jump = () => {
-    cube.current.applyImpulse({ x:0, y:7, z:0 });
+    cube.current.applyImpulse({ x:-5, y:2 , z:0 });
   };
-  useEffect(() => {
-    // 在客戶端渲染時初始化相關功能
-    // 例如：設置事件監聽器，初始化動畫，等等
-  }, []);
+
+  // const jumpPressed = useKeyboardControls((state) => state[Controls.jump]);
+
+  // useFrame((_state, delta) => {
+  //   if (jumpPressed) {
+  //     jump();
+  //   }
+  //   if (!start) {
+  //     return; 
+  //   }
+  // })
+  const isOnFloor = useRef(true); 
+  // useEffect(() => {
+  //   // 在客戶端渲染時初始化相關功能
+  //   // 例如：設置事件監聽器，初始化動畫，等等
+  // }, []);
   
   return (
     <>
@@ -25,8 +45,8 @@ export default function Experience() {
       <directionalLight position={[5,5,5]} intensity={0.8} castShadow color={"#9e69da"}/>
       {/* STAGE */}
       <RigidBody colliders={false} type="fixed" position-y={1}>
-        <CylinderCollider args={[0.5, 13.5]}/>
-        <Cylinder scale={[13.5,0.5,13.5]} receiveShadow>
+        <CylinderCollider args={[0.5, 20]}/>
+        <Cylinder scale={[20,0.5,20]} receiveShadow>
           <meshStandardMaterial color="white"/>
         </Cylinder>
       </RigidBody>
@@ -39,12 +59,8 @@ export default function Experience() {
           <meshStandardMaterial color={hover ? 'hotpink' : 'royalblue'} />
           </Box>
       </RigidBody>
-      {/* <RigidBody type="fixes" restitution={2}>
-        <Box position={[0,4,0]} args={[10,1,10]}>
-          <meshStandardMaterial color="springgreen"/>
-        </Box>
-      </RigidBody> */}
-      <GirlController onPointerEnter={() => setHover(true)}/>
+      <Ball/>
+      {/* <GirlController onPointerEnter={() => setHover(true)}/> */}
     </group>
    
     </>
