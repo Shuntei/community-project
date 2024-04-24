1be34ae2bc95d1b443ead62df2abc3be919120fb
林俊廷 <ms0491826@gmail.com>
2024-04-24 12:20:18 +0800

    '0434'

--- ruins-next/components/linda/navbar/navbar.js
+++ ruins-next/components/linda/navbar/navbar.js
@@ -87,11 +87,11 @@ export default function Navbar({ className, navColor = 'white' }) {
                         width={30}
                         className="rounded-full min-h-[30px] max-h-[30px] object-cover"
                         height={30}
-                        // src={
-                        //   auth.googlePhoto
-                        //     ? auth.profileUrl
-                        //     : `${IMG_SERVER}/${auth.profileUrl}`
-                        // }
+                        src={
+                          auth.googlePhoto
+                            ? auth.profileUrl
+                            : `${IMG_SERVER}/${auth.profileUrl}`
+                        }
                         alt=""
                       />
                     ) : (
@@ -159,11 +159,11 @@ export default function Navbar({ className, navColor = 'white' }) {
                 width={20}
                 className="rounded-full h-[20px] object-cover"
                 height={20}
-                // src={
-                //   auth.googlePhoto
-                //     ? auth.profileUrl
-                //     : `${IMG_SERVER}/${auth.profileUrl}`
-                // }
+                src={
+                  auth.googlePhoto
+                    ? auth.profileUrl
+                    : `${IMG_SERVER}/${auth.profileUrl}`
+                }
                 alt=""
               />
             ) : navColor === 'white' ? (
