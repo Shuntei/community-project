35905afa451f8571a7b5eb1f53535d8fa06b8811
Kevin <kevinlai29@outlook.com>
2024-05-05 17:47:32 +0800

    add terms confirm

--- ruins-next/pages/shop/cart/confirm-doc.js
+++ ruins-next/pages/shop/cart/confirm-doc.js
@@ -151,7 +151,10 @@ export default function ConfirmDoc() {
                     )}
                   </div>{' '}
                   <div className=" text-black text-[15px] font-normal font-['IBM Plex Mono']">
-                    物流方式:{purchaseOrder.row.shipping_method}
+                    物流方式:{purchaseOrder.row.shipping_method.split("(")[0]}
+                  </div>
+                  <div className=" text-black text-[15px] font-normal font-['IBM Plex Mono']">
+                    運費:免運費
                   </div>
                   <div className=" text-black text-[15px] font-normal font-['IBM Plex Mono']">
                     收件人:{purchaseOrder.row.recipient}
